86f719edd0135fc8d1a92553df4586307371c708
Pete Fullen <pfullen.code@gmail.com>
2017-12-18 20:43:19 -0500

    implementd read from txt

--- liri.js
+++ liri.js
@@ -5,7 +5,6 @@ const keys = require("./keys.js")
     //console.log(keys.twitterKeys);
 const twitterKeys = keys.twitterKeys;
 const spotifyKeys = keys.spotifyKeys;
-
 const Twitter = require('twitter');
 const Spotify = require('node-spotify-api');
 const request = require('request');
@@ -13,10 +12,9 @@ const fs = require('fs');
 const clear = require('clear');
 const CFonts = require('cfonts');
 const say = require('say')
-
 const clc = require('cli-color');
 process.stdout.write(clc.reset); // reset screen
-var googleTTS = require('google-tts-api');
+const googleTTS = require('google-tts-api');
 // set default font for cfonts
 
 var font = {
@@ -29,6 +27,15 @@ var font = {
     space: true, //define if the output text should have empty lines on top and on the bottom 
     maxLength: '0' //define how many character can be on one line 
 };
+
+
+// talk function
+
+function talkNow(message) {
+    setTimeout(function() {
+        say.speak(message);
+    });
+}
 
 
 // twitter
@@ -46,76 +53,57 @@ function getMyTweets() {
 
 function formatSpotifyResponse(response) {
     var items = response.items;
-    //console.log(items);
     items.forEach(function(album) {
-      //  console.log(album.name);
     })
 } // end of formatSpotifyResponse
 
 
 // spotify example
 function spotifyThis(query, queryS) {
-	console.log(query);
-	debugger;
+   // console.log(query);
     var spotify = new Spotify(spotifyKeys);
-//    console.log(queryS);
-   /* spotify.search({
+
+        
+      
+    spotify.search({
         type: 'track',
         query: query
     }, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log(data);
-        var response = data.tracks;
+        var tracks = data.tracks;
+        var items = tracks.items;
+        //console.log("THis is item" + items);
+        items.find(function(item) {
+            //console.log(item);
+
+            var albumInfos = item.album;
+
+            console.log("Album Name: " + albumInfos.name);
 
 
-        //console.log("\nHere are your " + command + " results:\n")
-        //formatSpotifyResponse(response);
-        //console.log(response); 
-     }); */
-     spotify.search({ type: 'track', query: query }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-
-var tracks = data.tracks;
-
-//console.log("Tracks " + tracks);
+            if (item.name === queryS) {
+                //console.log("This are the items" + item.album);
+                var artists = item.artists;
+                artists.forEach(function(artist) {
+                    console.log("Artist: " + artist.name);
+                })
 
 
+                //console.log(item);
+                console.log("Song Name: " + item.name);
+                console.log("Song Preview: " + item.preview_url + "\n");
 
-var items = tracks.items;
-	//console.log("THis is item" + items);
-items.find(function(item) {
-	//console.log(item);
+            }
 
-var albumInfos = item.album;
+        });
 
-console.log("Album Name: " + albumInfos.name);
-
-
-if (item.name === queryS) {
-	//console.log("This are the items" + item.album);
-	var artists = item.artists;
-	artists.forEach(function(artist) {
-	console.log("Artist: " + artist.name);
-	})
+        //	console.log("Song Name:" + item.name);
+        //	console.log("Preview URL:" + item.preview_url)
 
 
-	//console.log(item);
-	console.log("Song Name: "  + item.name);
-	console.log("Song Preview: " + item.preview_url + "\n");
-	
-}
-
-});
-	
-//	console.log("Song Name:" + item.name);
-//	console.log("Preview URL:" + item.preview_url)
-
-
-});
+    });
 }; // end of spotifyThis function 
 
 //spotify.search({ type: 'track', query: 'Somewhere over the rainbow' }, function(err, data,body ) {
@@ -130,24 +118,33 @@ if (item.name === queryS) {
 //});
 
 function getSpotifyFromTxt() {
-     console.log("Get the txt file")
-};
+  
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        } else {
+        	data = data.split(",");        	        	
+            console.log(data[1]);
+            data = data[1];
+            spotifyThis(data);
+        }
+    })
+}; // End of getSpotifyFromTxt function
 
+//   Start of Movie This Function 
 function movieThis(query) {
     process.stdout.write(clc.move.to(20, 25));
     // Then run a request to the OMDB API with the movie specified
     var queryUrl = "http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy";
-
     request(queryUrl, function(error, response, body) {
         //console.log(JSON.parse(body));
         var body = JSON.parse(body);
-
         if (!error && response.statusCode === 200 && body.Response !== "False") {
-           
-            say.speak("I found your movie");          
+            talkNow("I found your movie");
             console.log("\nHere are your " + command + " results:\n")
             console.log("Title: " + body.Title);
-            say.speak("Title: " + body.Title)
+            talkNow("Title: " + body.Title)
             console.log("Year: " + body.Year);
             var ratings = body.Ratings;
             ratings.forEach(function(rating) {
@@ -166,19 +163,20 @@ function movieThis(query) {
             console.log(clc.red("Please make sure to enter a movie\n\n\n\n\n\n"))
         }
         var plot = JSON.stringify(body.plot);
-        googleTTS('I found your Movie', 'en', 1)   // speed normal = 1 (default), slow = 0.24
-.then(function (url) {
-  console.log(url); // https://translate.google.com/translate_tts?...
-})
-.catch(function (err) {
-  console.error(err.stack);
-});
-    });
+    })
 }; // end of movieThis function 
+
+// format arg error messages 
+function processMsg(message) {
+    process.stdout.write(clc.move.to(40, 25));
+    console.log(clc.red(message));
+    process.stdout.write(clc.move.to(0, 50));
+}
 
 
 // Main program  
 CFonts.say('\t\tWelcome|to|Liri!', font);
+talkNow('Welcome to Leeri!');
 
 var args = process.argv;
 //console.log(args[2]);
@@ -186,9 +184,9 @@ var command = args[2];
 var query = "";
 var queryS = "";
 
- 
 
 
+// Make sure to capture all args
 for (var i = 3; i < args.length; i++) {
     if (i > 3 && i < args.length) {
         query = query + "+" + args[i];
@@ -196,40 +194,35 @@ for (var i = 3; i < args.length; i++) {
     } else {
         query += args[i];
         queryS += args[i];
-
     }
 }
 
-
 //var sQuery = JSON.stringify(query);
-//console.log(sQuery);
+
 switch (command) {
     case "my-tweets":
         getMyTweets();
         break;
     case "spotify-this-song":
-    	 if (query === "") {
-    		getSpotifyFromTxt();
-    	}else {
-        spotifyThis(query, queryS);
-    	}
-    	console.log("Hey" + query);
+        if (query === "") {
+            getSpotifyFromTxt();
+        } else {
+            spotifyThis(query, queryS);
+        }
+       
         break;
     case "movie-this":
         if (query === "") {
-        	console.log("Please enter a movie as a arg");
-        }else {
-        movieThis(query);
-    	}
+            processMsg("Please make sure to enter a movie as a arg");
+
+        } else {
+            movieThis(query);
+        }
         break;
-        case "do-what-it-says":
+    case "do-what-it-says":
         doWhatItSays();
         break;
-    case (!command):
-        console.log("Please enter a command argument");
+    case undefined:
+        processMsg("Please make sure to enter a command argument");
         break;
 }
-
-say.speak('Welcome to Leeri!');
-
- 
